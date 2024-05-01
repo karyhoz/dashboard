f85981917df4c1893de407f78f9fc097edef195e
Adeniyi Bayo <119003087+karyhoz@users.noreply.github.com>
2024-05-01 12:15:37 +0100

    Add files via upload

---
+++ oder.js
@@ -0,0 +1,20 @@
+const Orders = [
+    {
+        productName: 'karyhorz Gift Items',
+        productNumber: '26448',
+        paymentStatus: 'Due',
+        status: 'Pending'
+    },
+    {
+        productName: 'Karyhorz Branding',
+        productNumber: '35528',
+        paymentStatus: 'Refund',
+        status: 'Declined'
+    },
+    {
+        productName: 'Karyhorz Design',
+        productNumber: '49011',
+        paymentStatus: 'Paid',
+        status: 'Active'
+    },
+];
